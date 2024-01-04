feaab5cf3a784361ca7c91046ccb85d38dd51a0f
oelopes <oelopes2@gmail.com>
2024-01-04 18:45:29 -0300

    loading state

--- server.js
+++ server.js
@@ -6,19 +6,23 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get('/users', (req, res) => {
-  const users = [
-    { id: 1, name: 'Leo'},
-    { id: 2, name: 'Lao'},
-    { id: 3, name: 'Lio'}
-  ]
+app.get('/users', async (req, res) => {
+  // const users = [
+  //   { id: 1, name: 'Leo'},
+  //   { id: 2, name: 'Lao'},
+  //   { id: 3, name: 'Lio'}
+  // ]
 
-  res.send(`
+  setTimeout(async() => {
+    const users = await fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())
+
+    res.send(`
     <h1 class="text-2xl font-bold my-4">Users</h1>
     <ul>
       ${users.map(user => `<li>${user.name}</li>`).join('')}
     </ul>
   `);
+  }, 2000)
 });
 
 app.listen(3000, () => {
