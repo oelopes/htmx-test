4b1e116145f8feb1629144360377bfd0b400a4cb
oelopes <oelopes2@gmail.com>
2024-01-04 18:39:08 -0300

    get data from server route

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
-app.use(express.json)
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-app.listen(3000, () => console.log("Server running on 3000"))
+app.get('/users', (req, res) => {
+  const users = [
+    { id: 1, name: 'Leo'},
+    { id: 2, name: 'Lao'},
+    { id: 3, name: 'Lio'}
+  ]
+
+  res.send(`
+    <h1 class="text-2xl font-bold my-4">Users</h1>
+    <ul>
+      ${users.map(user => `<li>${user.name}</li>`).join('')}
+    </ul>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on 3000")
+});
